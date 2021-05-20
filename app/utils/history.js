466a84911bbdb5f230ece26fa4606099379c567c
Chetan Giri <chetangiri@Chetans-MacBook-Pro.local>
2021-05-20 18:11:08 +0530

    deploy website and cleanup

--- app/utils/history.js
+++ app/utils/history.js
@@ -1,4 +1,4 @@
 import { createBrowserHistory } from 'history';
-const baseUrl = process.env.NODE_ENV === 'production' ? '/react-template' : '/';
+const baseUrl = process.env.NODE_ENV === 'production' ? '/furniture-store-basic' : '/';
 const history = createBrowserHistory({ basename: baseUrl });
 export default history;
